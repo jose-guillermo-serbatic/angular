fc0c5bda43f1c25427683f656ad892f4a41922e3
jgbejar <jgbejar@serbatic.es>
2024-10-01 11:03:49 +0200

    no message

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,11 +1,14 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+// import typescriptLogo from './typescript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.ts'
 // import './topics/01-basic-types';
 // import './topics/02-object-interface.ts';
-import "./topics/03-function.ts";
-
+// import "./topics/03-function.ts";
+// import "./topics/04-homework-types";
+// import "./topics/05-basic-destructuring";
+// import "./topics/06-function-destructuring";
+import "./topics/07-import-export";
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   Hola mundo
 `
