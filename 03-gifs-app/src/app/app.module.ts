f59abdcb244601921ec78a1d32d08bab70cec4e5
jgbejar <jgbejar@serbatic.es>
2024-10-03 13:23:34 +0200

    no message

--- 03-gifs-app/src/app/app.module.ts
+++ 03-gifs-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 import { SharedModule } from './shared/shared.module';
 import { GifsModule } from './gifs/gifs.module';
@@ -17,7 +18,9 @@ import { AppComponent } from './app.component';
     GifsModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
